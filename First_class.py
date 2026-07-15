395bbc900087251705e70b89b79d4b553d53fddb
Pranam Kolhapure <pranamkolhapure@gmail.com>
2026-07-15 19:04:18 +0530

    Add files via upload

---
+++ First_class.py
@@ -0,0 +1,21 @@
+class Car:
+	def __init__(self, model, year, color, for_sale):
+		self.model = model
+		self.year = year
+		self.color = color
+		self.for_sale = for_sale
+
+
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", True)
+car3 = Car("Charger", 2026, "yellow", True)
+
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car1.for_sale)
+
+
+
+
+
